92ce8599b6c47976aeb55ac4bde44ee715a5f41a
Pratibhamaran <143260288+Pratibhamaran@users.noreply.github.com>
2025-08-24 16:28:19 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,7 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/empl?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "prati_empl";
